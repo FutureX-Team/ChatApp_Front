5d95c0d617babc7c3c952ad221095b9e49db75bf
AHMED ALFISAL <112659033+AHMED667830@users.noreply.github.com>
2025-09-02 01:43:34 +0300

    Delete src/components/buttonRegister.js

--- src/components/buttonRegister.js
+++
@@ -1,9 +0,0 @@
-function ButtonRegister({ onClick }) {
-  return (
-<button className="bg-blue-900 text-white p-2 rounded hover:bg-blue-500 transition-colors duration-300"
- onClick={ onClick }
- >تسجيل</button>
-  );
-}
-
-export default ButtonRegister;
